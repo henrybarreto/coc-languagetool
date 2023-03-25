29e78021bef1bfdf2b6c85c293001b60d4bb29c7
Henry Barreto <me@henrybarreto.dev>
2023-03-25 11:09:52 -0300

    wip

---
+++ README.md
@@ -0,0 +1,52 @@
+# coc-languagetool
+
+LanguageTool integration for coc.nvim
+
+## What is LanguageTool?
+[LanguageTool](https://languagetool.org/) is an open-source grammar and style 
+checker that can be used to help identify and correct errors in written text.
+It supports over 25 languages and provides feedback on a variety of potential 
+issues, including spelling errors, grammatical mistakes, and style 
+inconsistencies. LanguageTool can be integrated into a variety of writing 
+environments, including web browsers, word processors, and text editors, making
+it a versatile and powerful tool for anyone who wants to improve their writing 
+skills. With its customizable rules and support for multiple languages, 
+LanguageTool is a valuable resource for writers of all levels, from beginners 
+to professionals.
+
+## Why
+- Professionalism: Good grammar and writing skills can help make your code more 
+professional and easier to read. This can be especially important if you're 
+working on a team or sharing your code with others.
+
+- Clarity: Clear and concise writing can help ensure that your code is easy to 
+understand and follow, reducing the likelihood of bugs and errors.
+
+- Error Detection: LanguageTool can detect spelling and grammatical errors that
+might otherwise go unnoticed, helping you to catch and fix mistakes early on.
+
+- Internationalization: If you're writing code that will be used by people 
+around the world, good writing skills can be especially important for ensuring
+that your code is clear and understandable to everyone, regardless of their 
+native language.
+
+- Efficiency: Good writing skills can help you communicate your ideas more 
+effectively, potentially saving you time and effort in the long run.
+
+- Self-Improvement: Using LanguageTool to improve your grammar and writing skills
+can help you become a better communicator overall, both in your programming work
+and in other areas of your life.
+
+## Features
+coc-languagetool will provide multilingual grammar, style, and spell checker for:
+
+- [ ] Git related files
+- [ ] Comments
+and more!
+
+
+## Usage
+TODO: under development
+
+## License
+MIT
---
+++ src/index.ts
@@ -0,0 +1,110 @@
+import { workspace, commands, services } from "coc.nvim";
+import {
+	ExtensionContext,
+	LanguageClient,
+	LanguageClientOptions,
+	ServerOptions,
+	Document,
+} from "coc.nvim";
+import * as path from "path";
+
+const serverOptions: ServerOptions = {
+	module: path.join(__dirname, "server.js"),
+	args: [],
+	options: {
+		cwd: __dirname,
+	},
+};
+
+const clientOptions: LanguageClientOptions = {
+	documentSelector: [
+		"gitcommit",
+		"gitconfig",
+		"gitattributes",
+		"gitignore",
+		"git-rebase-todo",
+		"git-rebase",
+	],
+	synchronize: {
+		configurationSection: "languagetool",
+	},
+};
+
+const languageClient = new LanguageClient(
+	"languagetool",
+	"LanguageTool LSP",
+	serverOptions,
+	clientOptions,
+);
+
+export async function activate(context: ExtensionContext): Promise<void> {
+	const subscriptions = context.subscriptions;
+
+	subscriptions.push(services.registLanguageClient(languageClient));
+
+	subscriptions.push(
+		commands.registerCommand("languagetool.enable", async () => {
+			workspace.getConfiguration().update("languagetool.enable", true, true);
+		}),
+		commands.registerCommand("languagetool.disable", async () => {
+			workspace.getConfiguration().update("languagetool.enable", false, true);
+		}),
+		commands.registerCommand("languagetool.toggle", async () => {
+			const isEnabled = workspace
+				.getConfiguration()
+				.get<boolean>("languagetool.enable", true);
+			workspace
+				.getConfiguration()
+				.update("languagetool.enable", !isEnabled, true);
+		}),
+		commands.registerCommand("languagetool.check.line", async () => {}),
+		commands.registerCommand("languagetool.check.selection", async () => {
+			const doc: Document = await workspace.document;
+			workspace.getSelectedRange("v", doc).then((range) => {
+				if (!range) {
+					return;
+				}
+
+				languageClient.sendRequest("languagetool.check.selection", {
+					uri: doc.textDocument.uri,
+					range: {
+						start: {
+							line: range.start.line,
+							character: range.start.character,
+						},
+						end: {
+							line: range.end.line,
+							character: range.end.character,
+						},
+					},
+					selected: doc.textDocument.getText(range),
+				});
+			});
+		}),
+		commands.registerCommand("languagetool.check.document", async () => {}),
+		commands.registerCommand("languagetool.clear", async () => {
+			const doc = await workspace.document;
+			languageClient.sendNotification("languagetool.clear", {
+				textDocument: {
+					uri: doc.uri,
+				},
+			});
+		}),
+		commands.registerCommand("languagetoo.configure", async () => {
+			// const serverUrl = await workspace.requestInput('Enter the LanguageTool server URL');
+			// const language = await workspace.requestInput('Enter the language to use for checking');
+			// workspace.getConfiguration().update('LanguageTool.serverUrl', serverUrl, true);
+			// workspace.getConfiguration().update('LanguageTool.language', language, true);
+			// languageClient.sendNotification('LanguageToolConfigurationChanged', {
+			//     serverUrl,
+			//     language,
+			// });
+		}),
+	);
+
+	if (languageClient.needsStart()) {
+		languageClient.start();
+	}
+}
+
+export function deactivate(): void {}
